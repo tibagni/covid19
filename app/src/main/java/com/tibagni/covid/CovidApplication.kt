f11024cc0debff9424d6c4de84cdf06fada4fa86
Tiago Bagni <tibagni@gmail.com>
2020-07-18 11:26:06 -0300

    Add basic app center integration

--- app/src/main/java/com/tibagni/covid/CovidApplication.kt
+++ app/src/main/java/com/tibagni/covid/CovidApplication.kt
@@ -1,7 +1,21 @@
 package com.tibagni.covid
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CovidApplication: Application()
+class CovidApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCenter.start(
+            this,
+            BuildConfig.APP_CENTER_SECRET,
+            Analytics::class.java,
+            Crashes::class.java
+        )
+    }
+}
